3e11170a362f88ba4afb7ba6e14ee88bf4273730
DESKTOP-MUE9P6C\PC <nmhuyen.hcmus@gmail.com>
2018-04-20 09:43:00 +0700

    setting for drag and drop transition Vuejs

--- vuejs_demo/src/App.vue
+++ vuejs_demo/src/App.vue
@@ -1,32 +1,40 @@
+<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+
 <template>
   <div id="app">
     <router-view/>
-    <learn-energys></learn-energys>
-    <hello-world></hello-world>
+    <ul class="nav nav-stacked" style="list-style-type: none">
+          <li><router-link to="/learnEnergys">Go to LearnEnergys</router-link></li>
+          <li><router-link to="/">Go to Home Pages</router-link></li>
+    </ul>
   </div>
+
 </template>
 
 <script>
-import LearnEnergys from '@/components/LearnEnergys'
+import LearnEnergys from "@/components/LearnEnergys"
 
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from "@/components/HelloWorld"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     LearnEnergys
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+//@import "./assets/sass/main.scss"
+
 </style>
