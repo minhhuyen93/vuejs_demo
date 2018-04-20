b838ad0497ad0cc6fd4aafa76784ba199134888e
DESKTOP-MUE9P6C\PC <nmhuyen.hcmus@gmail.com>
2018-04-20 13:04:39 +0700

    install bootstrap and Vuex for state management

--- vuejs_demo/src/App.vue
+++ vuejs_demo/src/App.vue
@@ -1,12 +1,14 @@
 <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
 
 <template>
-  <div id="app">
-    <router-view/>
-    <ul class="nav nav-stacked" style="list-style-type: none">
-          <li><router-link to="/learnEnergys">Go to LearnEnergys</router-link></li>
-          <li><router-link to="/">Go to Home Pages</router-link></li>
+  <div id="app" class="col-md-12 col-lg-12">
+    <ul class="nav nav-stacked main-navigation-bar">
+          <li class="navigation-item"><router-link to="/learnEnergys">Go to LearnEnergys</router-link></li>
+          <li class="navigation-item"><router-link to="/">Go to Home Pages</router-link></li>
     </ul>
+    <div class="main-content">
+    <router-view/>
+    </div>
   </div>
 
 </template>
@@ -16,25 +18,45 @@ import LearnEnergys from "@/components/LearnEnergys"
 
 import HelloWorld from "@/components/HelloWorld"
 
+import BootstrapVue from 'bootstrap-vue'
+
 export default {
   name: "App",
   components: {
     HelloWorld,
-    LearnEnergys
+    LearnEnergys,
+    BootstrapVue
   }
 };
 </script>
 
 <style lang="scss" scoped>
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-//@import "./assets/sass/main.scss"
+.main-navigation-bar {
+  list-style-type: none;
+  text-align: center;
+  display: inline-flex;
+  top: 0;
+  float: left;
+}
+
+.navigation-item {
+  width: auto;
+  padding: 0 10px;
+}
+
+.main-content {
+  display: inline-block;
+  width: 100%;
+  margin-top: 60px;
+}
 
 </style>
--- vuejs_demo/src/components/LearnEnergys.vue
+++ vuejs_demo/src/components/LearnEnergys.vue
@@ -1,11 +1,101 @@
 <template>
-  <div >
-    <h1>Hello LearnEnergys Page</h1>
+  <div class="row col-lg-12">
+      <div class="column">
+        <span class="">NEW
+        </span>
+        <div class="container-fluid">
+          <div v-for="machine of machines" v-bind:key="machine.id">
+            <div class="container-fluid machine-contents">
+              <draggable v-model="machine.jobs">
+                <transition-group name="list-complete">
+                  <div v-for="job in machine.jobs" v-bind:key="job.jobNumber" class="list-complete-item">
+                    <div>{{ job.jobNumber }}</div>
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <span class="">PENDING
+        </span>
+        <div class="container-fluid">
+          <div v-for="machine of machines" v-bind:key="machine.id">
+            <div class="container-fluid machine-contents">
+              <draggable v-model="machine.jobs">
+                <transition-group name="list-complete">
+                  <div v-for="job in machine.jobs" v-bind:key="job.jobNumber" class="list-complete-item">
+                    <div>{{ job.jobNumber }}</div>
+                  </div>
+                </transition-group>
+              </draggable>
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+
 export default {
-  name: 'LearnEnergys'
+  name: 'LearnEnergys',
+  components: {
+    draggable
+  },
+  data: function () {
+    return {
+      machines: [
+        {
+          name: 'H1',
+          id: 1,
+          jobs: [
+            {
+              jobNumber: '4037-12'
+            },
+            {
+              jobNumber: '14038-13'
+            },
+            {
+              jobNumber: '14048-15'
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
+<style>
+.machine-contents {
+  width: 200px;
+  text-align: center;
+  display: inline-block;
+}
+.list-complete-item {
+    padding: 4px;
+    margin-top: 4px;
+    border: solid 1px;
+    transition: all 1s;
+  }
+  .list-complete-enter, .list-complete-leave-active {
+    opacity: 0;
+  }
+
+.column {
+    float: left;
+    width: 16.66%;
+    border-radius: 5px;
+    margin: 0 5px;
+    border: 1px solid;
+    height: 300px;
+}
+
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+</style>
