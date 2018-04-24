9f08d6cba1e4a26cc667dfcbff2b00af86554641
DESKTOP-MUE9P6C\PC <nmhuyen.hcmus@gmail.com>
2018-04-24 11:00:39 +0700

    move to Vue App file html

--- vuejs_demo/src/App.vue
+++ vuejs_demo/src/App.vue
@@ -1,11 +1,145 @@
 <template>
   <div id="app" class="col-md-12 col-lg-12">
-    <ul class="nav nav-stacked main-navigation-bar">
-        <li class="navigation-item"><router-link to="/learnEnergys">Go to LearnEnergys</router-link></li>
-        <li class="navigation-item"><router-link to="/">Go to HomePage</router-link></li>
-    </ul>
-    <div class="main-content">
-        <router-view></router-view>
+    <div class="left-container">
+      <div class="main-process">
+        <div class="process-item">
+            <h3 class="process-item__title">New</h3>
+            <i class="setting-icon"/>
+            <span class="process-item__number"> (3)</span>
+        </div>
+        <div class="process-item">
+            <h3 class="process-item__title">Pending</h3>
+            <i class="setting-icon"/>
+            <span class="process-item__number"> (5)</span>
+        </div>
+        <div class="process-item">
+            <h3 class="process-item__title">Scheduled</h3>
+            <i class="setting-icon"/>
+            <span class="process-item__number"> (4)</span>
+        </div>
+        <div class="process-item">
+            <h3 class="process-item__title">In Field</h3>
+            <i class="setting-icon"/>
+            <span class="process-item__number"> (2)</span>
+        </div>
+        <div class="process-item">
+            <h3 class="process-item__title">Compleled</h3>
+            <i class="setting-icon"/>
+            <span class="process-item__number"> (99) </span>
+        </div>
+      </div>
+      <div class="content-process">
+  <!--start block for new status -->
+        <div class="content-process-container">
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI234 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3213</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 3/4/2018</p>
+            </div>
+          </div>
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI235 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3214</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 5/4/2018</p>
+            </div>
+          </div>
+        </div>
+  <!-- end block for new status -->
+  <!--start block for new pending -->
+        <div class="content-process-container">
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI234 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3213</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 3/4/2018</p>
+            </div>
+          </div>
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI235 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3214</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 5/4/2018</p>
+            </div>
+          </div>
+        </div>
+  <!-- end block for PEDNING status -->
+  <!--start block for new SCHEDULED -->
+        <div class="content-process-container">
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI234 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3213</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 3/4/2018</p>
+            </div>
+          </div>
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI235 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3214</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 5/4/2018</p>
+            </div>
+          </div>
+        </div>
+  <!-- end block for SCHEDULED status -->
+  <!--start block for new IN FIELD -->
+        <div class="content-process-container">
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI234 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3213</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 3/4/2018</p>
+            </div>
+          </div>
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI235 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3214</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 5/4/2018</p>
+            </div>
+          </div>
+        </div>
+  <!-- end block for IN FIELD status -->
+  <!--start block for new COMPLETED -->
+        <div class="content-process-container">
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI234 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3213</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 3/4/2018</p>
+            </div>
+          </div>
+          <div class="content-item">
+            <i class="state-icon"/>
+            <span class="content-item__title"> SOI235 - Install</span>
+            <div class="content-item__detail">
+              <p>Comms4 - 3214</p>
+              <p>Powercord</p>
+              <p class="content-item__detail--due">Due: 5/4/2018</p>
+            </div>
+          </div>
+        </div>
+  <!-- end block for COMPLETED status -->
+      </div>
     </div>
   </div>
 </template>
@@ -25,24 +159,4 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-
-.main-navigation-bar {
-  list-style-type: none;
-  text-align: center;
-  display: inline-flex;
-  top: 0;
-  float: left;
-}
-
-.navigation-item {
-  width: auto;
-  padding: 0 10px;
-}
-
-.main-content {
-  display: inline-block;
-  width: 100%;
-  margin-top: 60px;
-}
-
 </style>
